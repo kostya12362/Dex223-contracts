ba0a808eed383fc5e310648da357bbc8423be267
gorbunovperm <gorbunovperm@gmail.com>
2025-03-12 17:05:45 +0300

    New spot price Oracle

--- contracts/dex-core/Dex223Oracle.sol
+++ contracts/dex-core/Dex223Oracle.sol
@@ -1,36 +1,40 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+import "./interfaces/IUniswapV3Pool.sol";
+
 contract Oracle {
 
-    // Returns Time-Weighted Average Price.
-    // secondsAgo - The time period in seconds for which we calculate the average price.
-    function getTwap(IUniswapV3Pool pool, uint32 secondsAgo) public view returns(uint256 priceX96) {
-        require(secondsAgo > 0, "Interval must be > 0");
-
-        uint32[] memory ago = new uint32[](2);
-        ago[0] = secondsAgo;
-        ago[1] = 0; // now
-
-        (int56[] memory tickCumulatives, ) = pool.observe(ago);
-
-        // calculation of the average tick over a period of time.
-        int56 tickDifference = tickCumulatives[1] - tickCumulatives[0];
-        int56 averageTick = tickDifference / int56(int32(secondsAgo));
-
-        priceX96 = TickMath.getSqrtRatioAtTick(averageTick);
+    function getSqrtPriceX96(address poolAddress) public view returns(uint160 sqrtPriceX96) {
+        IUniswapV3Pool pool;
+        pool = IUniswapV3PoolOracle(poolAddress);
+        (sqrtPriceX96,,,,,,,) = pool.slot0();
+        return sqrtPriceX96;
     }
 
-    function getPrice(uint32 secondsAgo, uint8 decimalsToken0, uint8 decimalsToken1) external view returns (uint256 price) {
-        uint256 sqrtPriceX128 = getTwap(secondsAgo);
-        uint256 priceX192 = FullMath.mulDiv(sqrtPriceX96, sqrtPriceX96, 1 << 64);
+    function getSpotPriceTick(address poolAddress) public view returns(int24 tick) {
+        IUniswapV3Pool pool;
+        pool = IUniswapV3PoolOracle(poolAddress);
+        (uint160 sqrtPriceX96,, tick,,,,, bool unlocked) = pool.slot0();
+        return tick;
+    }
 
-        if (decimalsToken0 >= decimalsToken1) {
-            uint256 decimalsFactor = 10 ** (decimalsToken0 - decimalsToken1);
-            price = FullMath.mulDiv(priceX192, decimalsFactor, 1 << 128);
-        } else {
-            uint256 decimalsFactor = 10 ** (decimalsToken1 - decimalsToken0);
-            price = FullMath.mulDiv(priceX192, 1, decimalsFactor << 128);
+    function getPrice() public view returns(uint256 price) {
+        int24 tick = getSpotPriceTick();
+
+        int24 absTick = tick >=0 ? tick : -tick;
+        uint256 price_ = 10**18;
+
+        for(int24 i = 0; i < absTick; i++) {
+            price_ = (price_ * 10001) / 10000;
         }
+
+        if (tick < 0) {
+            price = (10**36) / price_;
+        } else {
+            price = price_;
+        }
+
+        return price;
     }
 }
