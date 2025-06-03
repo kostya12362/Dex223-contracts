265b3b9b97836cbc8ec59c1184114339e49af998
Dexaran <dexaran820@gmail.com>
2025-06-03 20:10:35 +0100

    Auto-detect pool address without the need for POOL_CODE_HASH

--- contracts/dex-periphery/base/PoolAddress.sol
+++ contracts/dex-periphery/base/PoolAddress.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
+contract IFactory
+{
+    mapping(address => mapping(address => mapping(uint24 => address))) public getPool;
+}
+
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x2ed8d490a5fd6b96b6fc965619c116f0c4dba73ee522cfc1bb2a714f08eae839; // Updated for 0.7.6 compiled version with 5000 runs (EVM version Prague)
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0xf381c67d48673ceb8a17b53722647aee43310f100cd97f25bfcc628958372b8b; // Updated for 0.7.6 compiled version with 5000 runs (EVM version Prague)
     /// @notice The identifying key of the pool
     struct PoolKey {
         address token0;
@@ -29,9 +34,9 @@ library PoolAddress {
     /// @param factory The Uniswap V3 factory contract address
     /// @param key The PoolKey
     /// @return pool The contract address of the V3 pool
-    function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
+    function computeAddress(address factory, PoolKey memory key) internal view returns (address pool) {
         require(key.token0 < key.token1);
-        pool = address(
+        /*pool = address(
             uint256(
                 keccak256(
                     abi.encodePacked(
@@ -42,6 +47,15 @@ library PoolAddress {
                     )
                 )
             )
-        );
+        );*/
+        //pool = address(IFactory(factory).getPool[key.token0][key.token1][key.fee]);
+
+        bytes memory data = abi.encodeWithSelector(0x1698ee82, key.token0, key.token1, key.fee);
+        (bool success, bytes memory returnData) = factory.staticcall(data);
+
+        pool = abi.decode(returnData, (address));
+
+
+        //0x1698ee820000000000000000000000005b38da6a701c568545dcfcb03fcb875f56beddc40000000000000000000000005b38da6a701c568545dcfcb03fcb875f56beddc40000000000000000000000000000000000000000000000000000000000000bb8
     }
 }
